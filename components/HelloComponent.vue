9f64d6243f2ce4c8eaec68a12bc5773211122015
Catherine114 <shunaev80@mail.ru>
2022-03-26 15:53:25 +0300

    'project'

---
+++ components/HelloComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>ЭТО СУПЕР БЛОГ</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {},
+};
+</script>
+
+<style scoped>
+div {
+  height: 50vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+h1 {
+  color: #0017ff;
+  text-align: center;
+  font-family: Oswald, Arial;
+  font-size: 10vmin;
+  letter-spacing: 0vmin;
+  text-shadow: 1vmin 1vmin black;
+}
+
+</style>
